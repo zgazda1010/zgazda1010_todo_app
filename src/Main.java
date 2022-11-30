230cfb6e362acbbb67188fb66b6d5c5cffa47ff5
Gazda Zsolt <zgazda1010@gmail.com>
2022-11-30 16:49:10 +0100

    Implemented Main.printUsageInformation method

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@ public class Main {
 
   public static void main(String[] args) {
 
+    if (args[0].equals("todo")) {
+      printUsageInformation();
+    }
   }
 
+  public static void printUsageInformation() {
+    System.out.println("Command Line Todo application\n"
+        + "=============================\n"
+        + "Command line arguments:\n"
+        + "\t-l   Lists all the tasks\n"
+        + "\t-a   Adds a new task\n"
+        + "\t-r   Removes an task\n"
+        + "\t-c   Completes an task");
+  }
 }
